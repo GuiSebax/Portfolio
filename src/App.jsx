504ec62304c9b3496fdd162eb233478180d7e325
GuiSebax <guiclemente2003@hotmail.com>
2025-02-21 12:09:56 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
-import ParticleBackground from './components/Background/ParticleBackground'
-import Header from './components/Header/Header'
-import About from './components/About/About'
-import Ability from './components/Abilities/Ability'
-import Projects from './components/Projects/Projects'
-import Contact from './components/Contatc/Contact'
+import "./App.css";
+import ParticleBackground from "./components/Background/ParticleBackground";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Ability from "./components/Abilities/Ability";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contatc/Contact";
 function App() {
-
   return (
     <>
       <ParticleBackground />
@@ -18,7 +17,7 @@ function App() {
         <Contact />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
