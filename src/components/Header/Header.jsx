43bd0bea72b05683d1f7102b308c459049fcdcd2
GuiSebax <guiclemente2003@hotmail.com>
2025-06-08 14:06:51 -0300

    commits

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import './Header.css';
-import Typical from 'react-typical';
-import Navbar from '../Navbar/Navbar';
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import Typical from "react-typical";
+import Navbar from "../Navbar/Navbar";
 const Header = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollTop);
+    return () => window.removeEventListener("scroll", checkScrollTop);
+  }, [showScroll]);
+
   return (
     <header id="home" className="cabecalho">
       <div className="header_items">
@@ -14,9 +33,12 @@ const Header = () => {
               className="typical"
               loop={Infinity}
               steps={[
-                'Desenvolvedor Front-End', 1000,
-                'Desenvolvedor Full Stack', 1000,
-                'Desenvolvedor Mobile', 1000,
+                "Desenvolvedor Front-End",
+                1000,
+                "Desenvolvedor Full Stack",
+                1000,
+                "Desenvolvedor Mobile",
+                1000,
               ]}
             />
           </strong>
@@ -25,8 +47,17 @@ const Header = () => {
       </div>
 
       <div className="header_image">
-        <img src="./fotoeu.jpeg" alt="fotoeu" />
+        <img src="./perfil.jpg" alt="fotoeu" />
       </div>
+      {showScroll && (
+        <button
+          onClick={scrollTop}
+          className="scrollTopButton"
+          title="Voltar ao topo"
+        >
+          &#8593; {/* Unicode for upward arrow */}
+        </button>
+      )}
     </header>
   );
 };
